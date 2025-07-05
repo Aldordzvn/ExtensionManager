e7992d15bc1f0b7c83eba39c7d2d6899d01c3f6c
aldordzvn <aldordzvn2001@gmail.com>
2025-07-04 18:28:46 -0700

    diseño responsive y logica del front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   logoImg = 'images/logo.svg';
-  moonImg = 'images/icon-moon.svg';
+  themeImg = 'images/icon-moon.svg';
+  toggleThemeBool: boolean = false;
+
+  toggleTheme(){
+    this.toggleThemeBool = !this.toggleThemeBool;
+    if(this.toggleThemeBool){
+      document.body.classList.remove('light-theme');
+      document.body.classList.add('dark-theme');
+      this.themeImg = 'images/icon-sun.svg';
+      console.log(this.toggleThemeBool);
+    }else{
+      document.body.classList.remove('dark-theme');
+      document.body.classList.add('light-theme');
+      this.themeImg = 'images/icon-moon.svg';
+      console.log(this.toggleThemeBool);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExtensionsComponent } from './extensions/extensions.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { ExtensionsComponent } from './extensions/extensions.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
